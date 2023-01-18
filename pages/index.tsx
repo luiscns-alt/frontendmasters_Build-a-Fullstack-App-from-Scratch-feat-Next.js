4c907d1530b618e49dbe8320008849a4acd87b6a
Luis Carlos <luiscarlosnors@gmail.com>
2023-01-17 22:38:18 -0300

    feat: Gradient Background & Image Box & Title & Subtitle UI Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+import GradientLayout from '../components/gradientLayout'
+
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <GradientLayout
+      roundImage
+      color="green"
+      subtitle="profile"
+      title="Scott Moss"
+      description="15 public playlists"
+      image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
+    >
+      <div>home page</div>
+    </GradientLayout>
+  )
 }
 
 export default Home
