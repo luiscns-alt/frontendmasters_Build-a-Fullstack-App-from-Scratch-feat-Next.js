96b926a04b9e425959f63a4c10e498c8f9a26a8e
Luis Carlos <luiscarlosnors@gmail.com>
2023-01-18 22:18:01 -0300

    feat: Seed User Data

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,18 @@ import { Box, Text, Flex } from '@chakra-ui/layout'
 import { Image } from '@chakra-ui/react'
 import GradientLayout from '../components/gradientLayout'
 import prisma from '../lib/prisma'
+import { useMe } from '../lib/hooks'
 
 const Home = ({ artists }) => {
+  const { user } = useMe()
+
   return (
     <GradientLayout
       roundImage
       color="gray"
       subtitle="profile"
-      title="Scott Moss"
-      description="15 public playlists"
+      title={`${user?.firstName} ${user?.lastName}`}
+      description={`${user?.playlistsCount} public playlists`}
       image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
     >
       <Box color="white" paddingX="40px">
